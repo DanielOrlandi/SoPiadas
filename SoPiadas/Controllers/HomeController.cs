a89b1c9fea38ad3d3f054530418e20f67dd6cf96
DanielOrlandi <91284665+DanielOrlandi@users.noreply.github.com>
2021-09-23 16:07:05 -0300

    Add files via upload

---
+++ SoPiadas/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoPiadas.Models;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using SoPiadas.Data;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace SoPiadas.Controllers;
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
+
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Piada.ToListAsync());
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ SoPiadas/Models/Piada.cs
@@ -0,0 +1,15 @@
+﻿
+namespace SoPiadas.Models;
+public class Piada
+{
+    public int Id { get; set; }
+    public string Pergunta { get; set; }
+    public string Resposta { get; set; }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public Piada()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    {
+        
+    }
+}
